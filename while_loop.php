c1fb7add9c47cfb11777ed01209da44f5c6cf195
IsuruAhinsa <isurujayawickramat@gmail.com>
2019-10-20 17:59:11 +0530

    first commit

---
+++ while_loop.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="plugins/fontawesome/css/all.min.css">
+    <title>PHP While Loop</title>
+</head>
+<body>
+
+    <?php
+
+        /*while (condition is true) {
+            code to be executed;
+        }*/
+        $num = 1;
+
+        while ($num <= 5) {
+            echo "Hello" . "<br>";
+            $num++;
+        }
+
+        echo "<hr>";
+
+        $chama = 1;
+
+        while ($chama <= 5) {
+            echo $chama . "<br>";
+            $chama++;
+        }
+
+    ?>
+
+<script src="js/jquery-3.4.1.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
