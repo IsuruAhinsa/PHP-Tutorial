ebcc525e1a8d7a6c8621cdcd42bd6c89fb664599
IsuruAhinsa <isurujayawickramat@gmail.com>
2019-11-05 23:16:41 +0530

    first commit

---
+++ global_scope.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="plugins/fontawesome/css/all.min.css">
+    <title>PHP Global Scope</title>
+</head>
+<body>
+
+    <?php
+
+        $var = 10; // global scope
+
+        function myFunc() {
+            echo "Inside the function: " . $var; // generate an error
+        }
+
+        myFunc();
+
+        echo "<br>";
+
+        echo "Outside the function: " . $var;
+
+    ?>
+
+<script src="js/jquery-3.4.1.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
